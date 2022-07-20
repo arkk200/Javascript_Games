198facc1f42b7df51b5f580c5dc5276e9390e930
arkk200 <arkk20000@gmail.com>
2022-07-20 23:46:11 +0900

    Finally Made Text RPG

--- Text_RPG/main.js
+++ Text_RPG/main.js
@@ -54,6 +54,7 @@ class Game {
     }
     onGameMenuInput = (event) => {
         event.preventDefault();
+        const {hero} = this;
         const input = event.target['menu-input'].value;
         if (input === '1') { // 모험
             this.changeScreen('battle');
@@ -70,25 +71,49 @@ class Game {
             this.showMessage(`${this.monster.name} 몬스터와 마주쳤다.`); // 메세지 표시
 
         } else if (input === '2') { // 휴식
-
+            if(hero.hp >= hero.maxHp){
+                this.showMessage('체력이 충분히 올랐습니다.');
+            } else {
+                hero.hp = hero.maxHp;
+                this.updateHeroStat();
+            }
         } else if (input === '3') { // 종료
-
+            window.close();
         }
     }
     onBattleMenuInput = (event) => {
         event.preventDefault();
+        const {hero, monster} = this; // 설정했었던 hero, monster
         const input = event.target['battle-input'].value;
         if (input === '1') { // 공격
-            const { hero, monster } = this; // 설정했었던 hero, monster
             hero.attack(monster);
             monster.attack(hero);
-            this.showMessage(`${hero.att}의 데이지를 주고, ${monster.att}의 데미지를 받았다.`)
+            if(hero.hp <= 0){
+                this.showMessage(`${hero.lev} 레벨에서 전사하셨습니다. 새 캐릭터를 생성하세요.`);
+                this.quit();
+            } else if (monster.hp <= 0){
+                this.showMessage(`몬스터를 잡아 ${monster.xp} 경헙치를 얻었습니다.`);
+                hero.getXp(monster.xp);
+                this.monster = null;
+                this.changeScreen('game');
+            } else {
+                this.showMessage(`${hero.att}의 데이지를 주고, ${monster.att}의 데미지를 받았다.`)
+            }
             this.updateHeroStat();
             this.updateMonsterStat();
         } else if (input === '2') { // 회복
-
+            if(hero.hp < hero.maxHp){
+                hero.hp += 20;
+                if(hero.hp > hero.maxHp) hero.hp = hero.maxHp;
+            }
+            monster.attack(hero);
+            this.showMessage(`20을 회복하고 ${monster.att}의 데미지를 받았다.`)
+            this.updateHeroStat();
         } else if (input === '3') { // 도망
-
+            this.changeScreen('game');
+            this.showMessage('');
+            this.monster = null;
+            this.updateMonsterStat();
         }
     }
     updateHeroStat(){
@@ -109,7 +134,7 @@ class Game {
     }
     updateMonsterStat(){
         const {monster} = this;
-        if(hero === null) {
+        if(monster === null) {
             $monsterName.textContent = '';
             $monsterHp.textContent = '';
             $monsterAtt.textContent = '';
@@ -122,38 +147,59 @@ class Game {
     showMessage(text){
         $message.textContent = text;
     }
+    quit(){
+        this.hero = null;
+        this.monster = null;
+        this.updateHeroStat();
+        this.updateMonsterStat();
+        $gameMenu.removeEventListener('submit', this.onGameMenuInput);
+        $battleMenu.removeEventListener('submit', this.onBattleMenuInput);
+        this.changeScreen('start');
+        game = null;
+    }
 }
 
-class Hero {
-    constructor(game, name) {
+// 부모 클래스
+class Unit {
+    constructor(game, name, hp, att, xp){
         this.game = game;
         this.name = name;
-        this.lev = 1;
-        this.maxHp = 100;
-        this.hp = 100;
-        this.xp = 0;
-        this.att = 10;
+        this.maxHp = hp;
+        this.hp = hp;
+        this.att = att;
+        this.xp = xp;
     }
-    attack(target) {
+    attack(target){
         target.hp -= this.att;
+    }
+}
+
+// extends: 상속
+class Hero extends Unit{
+    constructor(game, name) {
+        super(game, name, 100, 10, 0);
+        this.lev = 1;
     }
     heal(monster) {
         this.hp += 20;
         this.hp -= monster.att;
     }
+    getXp(xp){
+        this.xp += xp;
+        if(this.xp >= this.lev * 15){
+            this.xp -= this.lev * 15;
+            this.lev += 1;
+            this.maxHp += 5;
+            this.att += 5;
+            this.hp = this.maxHp;
+            this.game.showMessage(`레벨이 올랐습니다. 레벨: ${this.lev}`)
+        }
+    }
 }
 
-class Monster {
+class Monster extends Unit{
     constructor(game, name, hp, att, xp) {
-        this.game = game;
-        this.name = name;
-        this.maxHp = 100;
-        this.hp = 100;
-        this.xp = 0;
-        this.att = 10;
-    }
-    attack(target) {
-        target.hp -= this.att;
+        super(game, name, hp, att, xp)
     }
 }
 
