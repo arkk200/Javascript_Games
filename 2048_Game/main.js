f663db0dd8afc9b771981fb3178ed7b00a2af887
arkk200 <arkk20000@gmail.com>
2022-07-28 22:18:53 +0900

    Setting, + documentFragment

---
+++ 2048_Game/main.js
@@ -0,0 +1,56 @@
+const $table = document.querySelector('#table');
+const $score = document.querySelector('#score');
+let data = [];
+
+function startGame() {
+    /*
+    documentFragment는 메모리 안에만 존재하는 가상의 태그이다
+    documentFragment를 쓰는 이유는 태그에 추가하는 횟수가 많아질수록
+    성능 문제가 생길 가능성이 높아지기에 가상의 태그에 추가하여
+    마지막으로 태그에 가상의 태그를 추가하는 것이다.
+    */
+    const $fragment = document.createDocumentFragment();
+    [1, 2, 3, 4].forEach(() => {
+        const rowData = [];
+        data.push(rowData);
+        const $tr = document.createElement('tr');
+        [1, 2, 3, 4].forEach(() => {
+            rowData.push(0);
+            const $td = document.createElement('td');
+            $tr.appendChild($td);
+        });
+        $fragment.appendChild($tr);
+    });
+    $table.appendChild($fragment);
+    put2ToRandomCell();
+    draw();
+}
+
+function put2ToRandomCell() {
+    const emptyCells = []; // 빈 셀의 좌표를 담는 배열
+    data.forEach((rowData, i) => {
+        rowData.forEach((cellData, j) => {
+            if(!cellData) { // cellData가 0이라면 (비어있다면)
+                emptyCells.push([i, j]);
+            }
+        });
+    });
+    const randomCell = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+    data[randomCell[0]][randomCell[1]] = 2;
+}
+
+function draw() {
+    data.forEach((rowData, i) => {
+        rowData.forEach((cellData, j) => {
+            const $target = $table.children[i].children[j];
+            if(cellData > 0){
+                $target.textContent = cellData;
+                $target.className = 'color-' + cellData;
+            } else {
+                $target.textContent = '';
+                $target.className = '';
+            }
+        });
+    });
+}
+startGame();
